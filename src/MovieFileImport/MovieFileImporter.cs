856e250d7863661cbeb41d6348ba16fd44d4940e
Holger Friedrich <holger.friedrich@bekk.no>
2014-10-29 00:25:49 +0100

    Added importer that reads movies from a file

---
+++ src/MovieFileImport/MovieFileImporter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using MovieImportService;
+using Newtonsoft.Json;
+using System.IO;
+using Microsoft.Framework.ConfigurationModel;
+
+namespace MovieFileImport
+{
+    public class MovieFileImporter : IMovieImporter
+    {
+        public IEnumerable<Movie> ImportMovies()
+        {
+            var pathToFileWithMovies = ReadPathToFileWithMoviesFromConfig();
+            string movieFile = File.ReadAllText(pathToFileWithMovies);
+            return JsonConvert.DeserializeObject<List<Movie>>(movieFile);
+        }
+
+        private static string ReadPathToFileWithMoviesFromConfig()
+        {
+            var configuration = new Configuration();
+            configuration.AddJsonFile("config.json");
+            return configuration.Get("Data:FileWithMovies:Path");
+        }
+    }
+}
